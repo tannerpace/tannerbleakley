d73ff48fc57d69e07af57c500e0dad5c05797162
TannerB <tannerpace@gmail.com>
2022-06-27 18:08:50 -0400

    id

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,56 +1,52 @@
 <script context="module">
-	import { browser, dev } from '$app/env';
+  import { browser, dev } from '$app/env';
 
-	export const hydrate = dev;
-	export const router = browser;
-	export const prerender = true;
+  export const hydrate = dev;
+  export const router = browser;
+  export const prerender = true;
 </script>
 
 <svelte:head>
-	<title>resume</title>
+  <title>resume</title>
 </svelte:head>
 
-<div class="content">
-	<a
-		class="download-link"
-		href="https://drive.google.com/file/d/1WIltaHkZxwX0Z9sCnH8nMzj6Cemuuw4r/view?usp=sharing"
-	>
-		<h4 class="card-title">
-			<i
-				><svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					><path
-						d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479 6.908l-4-4h3v-4h2v4h3l-4 4z"
-					/></svg
-				></i
-			> Resume
-		</h4>
-	</a>
+<div id="resume" class="content">
+  <a
+    class="download-link"
+    href="https://drive.google.com/file/d/1WIltaHkZxwX0Z9sCnH8nMzj6Cemuuw4r/view?usp=sharing"
+  >
+    <h4 class="card-title">
+      <i
+        ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+          ><path
+            d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479 6.908l-4-4h3v-4h2v4h3l-4 4z"
+          /></svg
+        ></i
+      > Resume
+    </h4>
+  </a>
 
-	<img class="resume-svg" src="resume.svg" alt="resume" />
+  <img class="resume-svg" src="resume.svg" alt="resume" />
 </div>
 
 <style>
-	.content {
-		padding: 1rem;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+  .content {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 
-	.card-title {
-		font-size: 1.5rem;
-		font-weight: bold;
-		margin-bottom: 0.5rem;
-	}
+  .card-title {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
 
-	.download-link {
-		display: block;
-		text-align: center;
-		margin-top: 1rem;
-	}
+  .download-link {
+    display: block;
+    text-align: center;
+    margin-top: 1rem;
+  }
 </style>
