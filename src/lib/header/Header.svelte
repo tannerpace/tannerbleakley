1db70a441faac6b06b2d5d0f07742c7200479e7a
TannerB <tannerpace@gmail.com>
2022-06-27 22:15:35 -0400

    header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -5,23 +5,27 @@
 </script>
 
 <header>
-  <div class="corner">
-    <a href="https://kit.svelte.dev">
-      <img src={favicon} alt="tanner bleakley" />
-    </a>
-  </div>
-
+  <div class="corner" />
   <nav>
     <svg viewBox="0 0 2 3" aria-hidden="true">
       <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
     </svg>
     <ul>
-      <li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-      <li class:active={$page.url.pathname === '/about'}>
-        <a sveltekit:prefetch href="/about">About</a>
+      <li class:active={$page.url.pathname === '/'}>
+        <a sveltekit:prefetch href="/">Welcome</a>
       </li>
-      <li class:active={$page.url.pathname === '/todos'}>
-        <a sveltekit:prefetch href="/todos">Todos</a>
+      <li class:active={$page.url.pathname === '/about'}>
+        <a sveltekit:prefetch href="/about">about</a>
+      </li>
+      <li class:active={$page.url.pathname === '/projects'}>
+        <a sveltekit:prefetch href="/projects">Projects</a>
+      </li>
+
+      <li class:active={$page.url.pathname === '/contact'}>
+        <a sveltekit:prefetch href="/contact">Contact</a>
+      </li>
+      <li class:active={$page.url.pathname === '/resume'}>
+        <a sveltekit:prefetch href="/resume">resume</a>
       </li>
     </ul>
     <svg viewBox="0 0 2 3" aria-hidden="true">
@@ -30,7 +34,16 @@
   </nav>
 
   <div class="corner">
-    <!-- TODO put something else here? github link? -->
+    <a
+      href="
+        https://github.com/tannerpace"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+        ><path
+          d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm2.218 18.616c-.354.069-.468-.149-.468-.336v-1.921c0-.653-.229-1.079-.481-1.296 1.56-.173 3.198-.765 3.198-3.454 0-.765-.273-1.389-.721-1.879.072-.177.312-.889-.069-1.853 0 0-.587-.188-1.923.717-.561-.154-1.159-.231-1.754-.234-.595.003-1.193.08-1.753.235-1.337-.905-1.925-.717-1.925-.717-.379.964-.14 1.676-.067 1.852-.448.49-.722 1.114-.722 1.879 0 2.682 1.634 3.282 3.189 3.459-.2.175-.381.483-.444.936-.4.179-1.413.488-2.037-.582 0 0-.37-.672-1.073-.722 0 0-.683-.009-.048.426 0 0 .46.215.777 1.024 0 0 .405 1.25 2.353.826v1.303c0 .185-.113.402-.462.337-2.782-.925-4.788-3.549-4.788-6.641 0-3.867 3.135-7 7-7s7 3.133 7 7c0 3.091-2.003 5.715-4.782 6.641z"
+        /></svg
+      >
+    </a>
   </div>
 </header>
 
@@ -51,12 +64,6 @@
     justify-content: center;
     width: 100%;
     height: 100%;
-  }
-
-  .corner img {
-    width: 2em;
-    height: 2em;
-    object-fit: contain;
   }
 
   nav {
@@ -102,7 +109,7 @@
     top: 0;
     left: calc(50% - var(--size));
     border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--accent-color);
+    border-top: var(--size) solid var(--accent-color-dark);
   }
 
   nav a {
@@ -111,7 +118,7 @@
     align-items: center;
     padding: 0 1em;
     color: var(--heading-color);
-    font-weight: 700;
+    font-weight: 500;
     font-size: 0.8rem;
     text-transform: uppercase;
     letter-spacing: 0.1em;
@@ -120,6 +127,6 @@
   }
 
   a:hover {
-    color: var(--accent-color);
+    color: var(--accent-color-dark);
   }
 </style>
